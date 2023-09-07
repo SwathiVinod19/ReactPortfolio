592454880eb739d3a5503f9caf4aaca506bd9fd2
SwathiVinod19 <swathi.vinod@gmail.com>
2023-09-07 18:49:27 -0400

    made changes

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
   const [currentTab, handleTabChange] = useState("about");
 
 
-  // This method is checking to see what the value of `currentTab` is. Depending on the value of currentPage, we return the corresponding component to render.
+  
   const renderTab = () => {
     if (currentTab === "About") {
       return <About />;
